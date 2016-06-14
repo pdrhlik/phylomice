752ac8e11cce49dbf5f5f1f46685cef55b0e7a51
Patrik Drhlík <patrioscraper@gmail.com>
2016-06-14 13:49:58 +1000

    add method for precomputing psi and psiinv

--- R/phylomice.R
+++ R/phylomice.R
@@ -15,3 +15,19 @@
 #' @importFrom Rcpp sourceCpp
 #' @importFrom Rcpp evalCpp
 NULL
+
+#' Precomputes psi and psiinv given a tree.
+#'
+#' @param tree Phylogenetic tree that can be processed
+#' by the ape library.
+#' @return Returns a list with components psi
+#' and psiinv that are needed in phylogenetic
+#' imputation methods.
+#' @export
+precomputePsi <- function(tree) {
+  psi <- ape::vcv(tree)
+  return(list(
+    psi = psi,
+    psiinv = chol2inv(chol(psi))
+  ))
+}
